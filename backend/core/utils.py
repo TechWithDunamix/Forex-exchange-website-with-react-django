c6c6ee06a83c7c990b34e8284499145fc283b46e
techwithdunamix <techwithdunamix@gmail.com>
2024-08-05 17:58:08 +0100

    added templates for emailing

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.backends import BaseBackend
 from .models import User
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 class Authenticator(BaseBackend):
 
 	def authenticate(self,request,email = None,password = None):
@@ -21,3 +24,43 @@ class Authenticator(BaseBackend):
 
 		except:
 			return None
+		
+
+
+
+
+
+def send_withdrawal_confirmation_email(to_email, username, amount, account_url):
+    context = {
+        'username': username,
+        'amount': amount,
+        'account_url': account_url
+    }
+    html_content = render_to_string('withdrawal.html', context)
+    text_content = strip_tags(html_content)  # Fallback to plain text email
+
+    email = EmailMultiAlternatives(
+        'Withdrawal Confirmed',
+        text_content,
+        'exchange7@gmail.com',
+        [to_email]
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+def send_deposit_confirmation_email(to_email, username, amount, account_url):
+    context = {
+        'username': username,
+        'amount': amount,
+        'account_url': account_url
+    }
+    html_content = render_to_string('deposit.html', context)
+    text_content = strip_tags(html_content)  # Fallback to plain text email
+
+    email = EmailMultiAlternatives(
+        'Deposit Confirmed',
+        text_content,
+        'exchange7@gmail.com',
+        [to_email]
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send()
